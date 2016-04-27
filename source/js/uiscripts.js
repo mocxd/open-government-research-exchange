46a75efb07133e1855759cd602380bdf303a4d2a
mocxd <mark.adkins.hastings@gmail.com>
2016-04-27 14:24:17 -0400

    adds new tools filter, updates macro

--- source/js/uiscripts.js
+++ source/js/uiscripts.js
@@ -3,12 +3,6 @@ $(function() {
     $('#clear-filter-btn').click(function() {
         $( document ).trigger( 'filter:clearRebind' );
         $('#lunr-filter').val('');
-        // $('#filter-organization').val('*').change();
-        // $('#filter-objective').val('**').change();
-        // $('#filter-methodology').val('***').change();
-        // $('#filter-sector').val('****').change();
-        // $('#filter-region').val('*****').change();
-        // $('#filter-type').val('******').change();
 
         $('select.b-filter option').prop('selected', false).removeClass('m-active');
         $("#filter-organization option[data-filter='*']").prop('selected', true).addClass('m-active');
@@ -17,5 +11,6 @@ $(function() {
         $("#filter-sector option[data-filter='****']").prop('selected', true).addClass('m-active');
         $("#filter-region option[data-filter='*****']").prop('selected', true).addClass('m-active');
         $("#filter-type option[data-filter='******']").prop('selected', true).addClass('m-active');
+        $("#filter-tools option[data-filter='*******']").prop('selected', true).addClass('m-active');
     });
 });
